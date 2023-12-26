2bb51620259b70eaba5c0b4529b8e970d8b5ab82
unknown <adityaraj715426@gmail.com>
2023-12-26 22:52:50 +0530

    build the mongo without jwt

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,19 +1,28 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect('mongodb+srv://rajaditya0thakur:dq0P3wkxNQt1sjFS@cluster0.vyrcpnp.mongodb.net/Assign');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    username : {type : String , required : true} , 
+    password : {type : String , required : true} , 
 });
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    username : {type : String , required : true} , 
+    password : {type : String , required : true} , 
+
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    title : {type : String , required : true} , 
+    description : {type : String , required : true} , 
+    price : {type : Number  , required  : true} , 
+    imageLink : {type  : String } , 
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
