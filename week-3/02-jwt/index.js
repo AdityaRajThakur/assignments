00b91be705df5bfbc847d3b1eb4d84637dfac92f
unknown <adityaraj715426@gmail.com>
2023-12-26 14:01:43 +0530

    assignment are completed expcept, jwt-mongo

--- week-3/02-jwt/index.js
+++ week-3/02-jwt/index.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const zod = require('zod'); 
 const jwtPassword = 'secret';
 
 
@@ -15,6 +16,11 @@ const jwtPassword = 'secret';
  */
 function signJwt(username, password) {
     // Your code here
+    const usernameSchema = zod.string().email() ; 
+    const passwordSchema = zod.string().min(6);
+    if(!usernameSchema.safeParse(username)['success'] || !passwordSchema.safeParse(password)['success']) return null ; 
+    const token = jwt.sign({username , password} ,jwtPassword )
+    return token ; 
 }
 
 /**
@@ -27,6 +33,12 @@ function signJwt(username, password) {
  */
 function verifyJwt(token) {
     // Your code here
+    try{
+        const res = jwt.verify(token , jwtPassword) ; 
+        return true ; 
+    }catch(E){
+        return false ; 
+    }
 }
 
 /**
@@ -38,6 +50,9 @@ function verifyJwt(token) {
  */
 function decodeJwt(token) {
     // Your code here
+    const decoded_token = jwt.decode(token) ; 
+    if(decoded_token) return true;  
+    return false ;
 }
 
 
