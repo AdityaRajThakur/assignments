7575bdefc9ef5deee7892688fee44c073025350a
unknown <adityaraj715426@gmail.com>
2023-12-29 13:13:59 +0530

    added the jwt

--- week-3/04-mongo-with-jwt-auth/db/index.js
+++ week-3/04-mongo-with-jwt-auth/db/index.js
@@ -6,14 +6,26 @@ mongoose.connect('mongodb+srv://rajaditya0thakur:dq0P3wkxNQt1sjFS@cluster0.vyrcp
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    username : {type : String , required : true} , 
+    password : {type : String , required : true} , 
 });
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    username : {type : String , required : true} , 
+    password : {type : String , required : true} , 
+    purchasedCourse :[{
+        type : mongoose.Schema.ObjectId,
+        ref : "Course" , 
+    }]
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    title :{type : String , required : true} , 
+    description :{type :String, required : true} , 
+    price :{type : Number , required : true} , 
+    imageLink : {type  : String } , 
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
--- week-3/04-mongo-with-jwt-auth/middleware/admin.js
+++ week-3/04-mongo-with-jwt-auth/middleware/admin.js
@@ -1,7 +1,18 @@
+const jwt = require('jsonwebtoken') 
 // Middleware for handling auth
 function adminMiddleware(req, res, next) {
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+    const jwtPassword = "pass" ; 
+    const token = req.headers['authorization'] ; 
+    try{
+        const value = jwt.verify(token , jwtPassword) ; 
+        next() ; 
+    }catch(e){
+        return res.status(404).json({
+            "msg":"Authentication faild" , 
+        })
+    }
 }
 
 module.exports = adminMiddleware;
